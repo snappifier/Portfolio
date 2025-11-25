c51f6470c176b73fc7128e9803bf58ed34f0d9e4
snappify <snappifyacc@gmail.com>
2025-11-26 00:28:13 +0100

    scrollbar bug fix

--- app/scrollAnimation/ScrollAnimation.js
+++ app/scrollAnimation/ScrollAnimation.js
@@ -1,16 +1,37 @@
 "use client"
 
 import {motion, useScroll, animate, useTransform } from "motion/react"
-import {useRef} from "react"
+import {useEffect, useRef} from "react"
 
 export default function ScrollAnimation() {
 	const { scrollYProgress } = useScroll()
 	const ref = useRef(null)
+	const animationRef = useRef(null)
 	const width = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
+
+	useEffect(() => {
+	const stopAnimation = () => {
+		if (animationRef.current) {
+			animationRef.current.stop()
+			animationRef.current = null
+		}
+	}
+
+	window.addEventListener("wheel", stopAnimation)
+	window.addEventListener("touchstart", stopAnimation)
+
+	return () => {
+		window.removeEventListener("wheel", stopAnimation)
+		window.removeEventListener("touchstart", stopAnimation)
+		// Czyścimy animację przy odmontowaniu komponentu
+		if (animationRef.current) animationRef.current.stop()
+	}
+}, [])
 
 	const handleClick = (e) => {
 		const progress = ref.current
 		if (!progress) return
+		if (animationRef.current) animationRef.current.stop()
 
 		const {width: rectWidth, left} = progress.getBoundingClientRect()
 		const clickX = e.clientX - left
@@ -18,7 +39,7 @@ export default function ScrollAnimation() {
 		const totalScrollableHeight = document.documentElement.scrollHeight - window.innerHeight
 		const targetY = totalScrollableHeight * ratio
 
-		animate(window.scrollY, targetY, {
+		animationRef.current = animate(window.scrollY, targetY, {
 			type: "spring",
 			damping: 20,
 			stiffness: 60,
@@ -26,6 +47,9 @@ export default function ScrollAnimation() {
 
 			onUpdate: latest => {
 				window.scrollTo(0, latest)
+			},
+			onComplete: () => {
+				animationRef.current = null
 			}
 		})
 	}
@@ -34,13 +58,13 @@ export default function ScrollAnimation() {
 		<div
 			ref={ref}
 			onClick={handleClick}
-			className="fixed top-0 left-0 w-full h-2 cursor-pointer backdrop-blur-sm backdrop-brightness-75"
+			className="fixed top-0 left-0 w-screen h-2 cursor-pointer backdrop-blur-sm backdrop-brightness-75 z-999"
 		>
 			<motion.div
 				className="h-full overflow-hidden"
 				style={{width}}
 			>
-				<div className="h-full w-screen bg-linear-to-r from-indigo-500 via-blue-500 to-red-500" />
+				<div className="h-full w-screen bg-linear-to-r from-indigo-500 via-blue-500 to-cyan-500" />
 			</motion.div>
 		</div>
 	)
