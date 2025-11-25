a71fd4bced3d257009056f6d3c21fcfa56a0e84f
snappify <snappifyacc@gmail.com>
2025-11-25 22:47:53 +0100

    add main

---
+++ app/main/Main.js
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Main() {
+
+	return (
+		<div className="w-full h-screen flex items-center justify-center bg-zinc-950">
+			<div className="h-full w-1/2 bg-conic-90 from-[#F8F8F8] from-0% via-black via-0% to-[#AAAAAA] to-99% "></div>
+			<div className="h-full w-1/2 bg-conic-90 from-[#F8F8F8] from-0% via-black via-0% to-[#AAAAAA] to-99% scale-x-[-1]"></div>
+		</div>
+	)
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import ScrollAnimation from "@/app/scrollAnimation/ScrollAnimation";
+import Main from "@/app/main/Main";
 
 export default function Home() {
   return (<>
 		  <ScrollAnimation />
-    <div className="w-full h-2000 flex flex-col justify-around items-center bg-zinc-950">
+		  <Main />
+      <div className="w-full h-2000 flex flex-col justify-around items-center bg-zinc-950">
+
 	    <div className="w-1/2 h-100 bg-red-500"></div>
 	    <div className="w-1/2 h-100 bg-green-500"></div>
 	    <div className="w-1/2 h-100 bg-zinc-800"></div>
