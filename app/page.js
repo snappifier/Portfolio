26d38a1a57255def68dbc55a5f903d53bda93b65
snappify <snappifyacc@gmail.com>
2025-11-25 21:04:47 +0100

    scrollbar bug fix

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import ScrollAnimation from "@/app/scrollAnimation/ScrollAnimation";
 export default function Home() {
   return (<>
 		  <ScrollAnimation />
-    <div className="w-full h-2000 flex flex-col justify-around items-center bg-zinc-900">
+    <div className="w-full h-2000 flex flex-col justify-around items-center bg-zinc-950">
 	    <div className="w-1/2 h-100 bg-red-500"></div>
 	    <div className="w-1/2 h-100 bg-green-500"></div>
 	    <div className="w-1/2 h-100 bg-zinc-800"></div>
--- app/scrollAnimation/ScrollAnimation.js
+++ app/scrollAnimation/ScrollAnimation.js
@@ -1,19 +1,20 @@
 "use client"
 
-import {motion, useScroll, animate } from "motion/react"
+import {motion, useScroll, animate, useTransform } from "motion/react"
 import {useRef} from "react"
 
 export default function ScrollAnimation() {
 	const { scrollYProgress } = useScroll()
 	const ref = useRef(null)
+	const width = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
 
 	const handleClick = (e) => {
 		const progress = ref.current
 		if (!progress) return
 
-		const {width, left} = progress.getBoundingClientRect()
+		const {width: rectWidth, left} = progress.getBoundingClientRect()
 		const clickX = e.clientX - left
-		const ratio = clickX / width
+		const ratio = clickX / rectWidth
 		const totalScrollableHeight = document.documentElement.scrollHeight - window.innerHeight
 		const targetY = totalScrollableHeight * ratio
 
@@ -36,8 +37,11 @@ export default function ScrollAnimation() {
 			className="fixed top-0 left-0 w-full h-2 cursor-pointer backdrop-blur-sm backdrop-brightness-75"
 		>
 			<motion.div
-				className="h-full bg-sky-700 origin-left"
-				style={{scaleX: scrollYProgress}}></motion.div>
+				className="h-full overflow-hidden"
+				style={{width}}
+			>
+				<div className="h-full w-screen bg-linear-to-r from-indigo-500 via-blue-500 to-red-500" />
+			</motion.div>
 		</div>
 	)
 }
