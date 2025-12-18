910d313930c7db57e18576f987936d0ed78fcc76
snappifier <snappifyacc@gmail.com>
2025-12-18 17:53:09 +0100

    layout shift fix

--- app/main/Main.js
+++ app/main/Main.js
@@ -4,7 +4,7 @@ import Title from "@/app/main/Title";
 export default function Main({startAnimation}) {
 
 	return (<>
-			<div className="w-full h-svh min-h-[500px] sm:min-h-[600px] flex items-center justify-center bg-zinc-950 overflow-hidden">
+			<div className="w-full h-svh min-h-[500px] sm:min-h-[600px] flex items-center justify-center bg-zinc-950 overflow-hidden" style={{contain: "layout style paint", contentVisibility: "auto"}}>
 				<Title startAnimation={startAnimation}/>
 				<Glow startAnimation={startAnimation}/>
 			</div>
