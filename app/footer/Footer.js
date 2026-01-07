78b0e4f1a18d8137a777b584b1a0dbdc986f0944
snappify <snappifyacc@gmail.com>
2026-01-07 21:05:56 +0100

    footer text change

--- app/footer/Footer.js
+++ app/footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 
 	return (
 		<div className="absolute bottom-0 left-0 w-full text-xs sm:text-sm md:text-base  h-20 lg:h-25 flex items-center justify-center bg-zinc-950/95 text-white z-500 text-center px-5">
-			&copy;designed and developed by Krystian Matwiej
+			Designed and developed by Krystian Matwiej
 		</div>
 	)
 }
